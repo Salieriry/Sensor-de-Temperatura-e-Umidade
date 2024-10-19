16a33d8f634bfcd18779f5d7d8fc279684ebb2d4
salieri77 <hades666mdan@gmail.com>
2024-10-18 21:19:32 -0300

    Adicionar arquivos de projeto.

---
+++ Sensor de Temperatura e Umidade/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sensor_de_Temperatura_e_Umidade
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
